a656650b1d371ba3f37e5a4afa72d726884927d0
john <john@swarmee.net>
2018-04-26 05:43:53 +1000

    updates

--- name-address-parsing-api/python-api/python-api.py
+++ name-address-parsing-api/python-api/python-api.py
@@ -1,64 +1,95 @@
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask, url_for, jsonify
-from elasticsearch import Elasticsearch
+import probablepeople as pp
+import json
+from postal.parser import parse_address
 
-es_host = {"host": "elasticsearch1", "port": 9200}
-es = Elasticsearch([es_host], retry_on_timeout=True, maxsize=25)
 
 app = Flask(__name__)
 api = Api(app,
-	      version='1.0', 
-          title='Get Product And \"like\" Product Details', 
-          description='Simple Product API Endpoints', 
+        version='1.0', 
+          title='Swagger Test Page for Name and Address Parsing APIs', 
+          description='Name and Address Parsing', 
           prefix="/v1",
           contact="john@swarmee.net",
           contact_url="www.swarmee.net"
          )
 app.wsgi_app = ProxyFix(app.wsgi_app)
-ns = api.namespace('product', description='Simple Product API operations')
 
-@ns.route('/search/<searchTerms>')
-class productSearch(Resource):
-    def get(self, searchTerms):
-        productSearchResponse = es.search(index="products", body="{\"query\": {\"query_string\": {\"query\": \"%s\"}}}" % searchTerms)
-        productSearchClientResponse = []
-        for row in productSearchResponse["hits"]["hits"]:
-          productSearchClientResponse.append(row["_source"])
-        return jsonify(productSearchClientResponse)       
+pns = api.namespace('partyType', description='Simple Party Parser and Typer')
 
-@ns.route('/<productNumber>')
-class productNumber(Resource):
-    def get(self, productNumber):
-        productNumberSearchResponse = es.search(index="products", body="{\"query\": {\"match\": {\"_id\": \"%s\"}}}" % productNumber)
-        productNumberClientResponse = []
-        for row in productNumberSearchResponse["hits"]["hits"]:
-          productNumberClientResponse.append(row["_source"])
-        return jsonify(productNumberClientResponse)       
+partyName = pns.parser()
+partyName.add_argument('partyName', type=str, help='partyName for Processing', location='form')
 
-@ns.route('/like/<productNumber>')
-class likeProductNumber(Resource):
-    def get(self, productNumber):
-        likeProductNumberSearchResponse = es.search(index="products", body="{\"query\": {\"match\": {\"_id\": \"%s\"}}}" % productNumber, filter_path=['hits.hits._source.productName'])
-        for row in likeProductNumberSearchResponse["hits"]["hits"]:
-          likeProductNumberClientResponse = row["_source"]["productName"]
-        resp3 = es.search(index="products", body="{\"query\": { \"common\": {\"street\": {\"query\": \"%s\",\"cutoff_frequency\": 0.1}}}}" % likeProductNumberClientResponse, filter_path=['hits.hits._source.*'])
-        finalResponse = []
-        for row in resp3["hits"]["hits"]:
-          finalResponse.append(row["_source"])
-        return jsonify(finalResponse)       
+ans = api.namespace('addressParser', description='Simple Party Type Identification')
 
-@ns.route('/count')
-class productCount(Resource):
-    def get(self):
-        resp = es.count(index="products", filter_path=['-took','-timed_out','-_shards'])
-        return resp
+addressText = ans.parser()
+addressText.add_argument('addressText', type=str, help='The address to be parsed into components', location='form')
 
-@ns.route('/health')
-class elasticHealth(Resource):
-    def get(self):
-        resp = es.cluster.health(filter_path=['status'])
-        return resp
+
+##### Using Probable Name to determine party type
+@pns.route('/partyType')
+class partyType(Resource):
+    @pns.doc(parser=partyName)
+    def post(self):
+        args = partyName.parse_args()
+        partyNameText = args['partyName']
+        resp = pp.tag(partyNameText)
+        type = resp[-1]
+        if   type in ['Person']:
+          return jsonify(partyName=partyNameText,partyTypeConfidence='High',partyType=type)
+        elif type in ['Corporation']:
+          return jsonify(partyName=partyNameText,partyTypeConfidence='High',partyType=type)
+        elif type in ['Household']:
+          return jsonify(partyName=partyNameText,partyTypeConfidence='Medium',partyType=type)
+        else: 
+          return jsonify(partyName=partyNameText,partyTypeConfidence='Low',partyType='Person')
+
+##### Using Probable Name to determine party type
+@pns.route('/nameParser')
+class partyType(Resource):
+    @pns.doc(parser=partyName)
+    def post(self):
+        args = partyName.parse_args()
+        partyNameText = args['partyName']
+        resp0 = pp.tag(partyNameText)
+        type = resp0[-1]
+        finalResponse = dict(resp0[0])
+        partyType = {}
+        if   type in ['Person']:
+          partyType['partyTypeConfidence'] = 'High'
+          partyType['partyType'] = type
+        elif type in ['Corporation']:
+          partyType['partyTypeConfidence'] = 'High'
+          partyType['partyType'] = type
+        elif type in ['Household']:
+          partyType['partyTypeConfidence'] = 'Medium'
+          partyType['partyType'] = type
+        else:
+          partyType['partyTypeConfidence'] = 'Low'
+          partyType['partyType'] = 'Person'
+        return jsonify(submittedPartyName=partyNameText,parsedParty=finalResponse,partyType=partyType)
+ 
+
+
+##### Libpostal Address Parsing 
+@ans.route('/')
+class addressParser(Resource):
+    @ans.doc(parser=addressText)
+    def post(self):
+        args = addressText.parse_args()
+        fullAddressText = args['addressText']
+        resp = parse_address(fullAddressText)
+        finalResponse = dict((y, x) for x, y in resp)
+#        finalResponse = {}
+#        for loopera in resp:
+#          key = loopera[1] 
+#          value = loopera[0] 
+#          finalResponse[key] = value
+        return jsonify(finalResponse)
+ 
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
