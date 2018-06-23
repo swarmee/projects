cfdff64ef3e4d755678171a01ed2cb1ef5a49d5d
Unknown <john@swarmee.net>
2018-06-24 09:19:58 +1000

    sample 6.3 elastic stack

---
+++ elastic-stack-6.3/load-sample-data/load-sample-data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Wait for Elasticsearch to start up before doing anything.
+until curl -s http://elasticsearch:9200/_cat/health -o /dev/null; do
+    echo Waiting for Elasticsearch...
+    sleep 1
+done
+
+
+# Wait for Kibana to start up before doing anything.
+##until curl -s http://kibana1:5601/login -o /dev/null; do
+##    echo Waiting for Kibana...
+##    sleep 1
+##done
+
+
+# load mapping
+curl -H 'Content-Type: application/json' -XPUT 'http://elasticsearch:9200/real-estate-sales?pretty' -d @/tmp/sample/real-estate-sales.mapping.json
+
+# load data
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'http://elasticsearch:9200/real-estate-sales/sales/_bulk?pretty' --data-binary @/tmp/sample/real-estate-sales.sample.data.json
+
+echo 'Templates all loaded up'
